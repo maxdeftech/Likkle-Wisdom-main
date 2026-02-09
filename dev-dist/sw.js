e5a327fa2eeb4b7609bfdc982a25f18b187aa11b
maxwelldefinitivetechnologies <maxwellinnovativetechnologies@gmail.com>
2026-02-09 12:29:18 -0500

    changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-1e60b829'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8dd0je8t0gg"
+    "revision": "0.hqq1ln5ihto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
